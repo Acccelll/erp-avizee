9634da93b9d0d767e956449a78a8f279abb4c0fb
Acccelll <acelerador.to@gmail.com>
2026-03-19 17:09:49 -0300

    Refactored Dashboard component updated with ModulePage pattern and new features.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { ModulePage } from 'some-module'; // Import ModulePage pattern
-import GlobalPeriodFilter from 'components/GlobalPeriodFilter';
-import KPICards from 'components/KPICards';
-import OperationalInsights from 'components/OperationalInsights';
+import ModulePage from '../components/ModulePage';
+import PeriodFilter from '../components/PeriodFilter';
+import KPICards from '../components/KPICards';
+import AlertCard from '../components/AlertCard';
+import PieChart from '../components/PieChart';
 
 const Dashboard = () => {
-    return (
-        <ModulePage>
-            <GlobalPeriodFilter />
-            <KPICards />
-            <OperationalInsights title="Pendências do Dia" />
-        </ModulePage>
-    );
+  return (
+    <ModulePage>
+      <PeriodFilter />
+      <KPICards />
+      <div className="alerts">
+        <AlertCard title="Aguardando Faturamento" />
+        <AlertCard title="Compras Aguardando" />
+        <AlertCard title="Estoque Mínimo" />
+      </div>
+      <section className="ultimos-orcamentos">
+        <h2>Últimos Orçamentos</h2>
+      </section>
+      <section className="resumo-geral">
+        <h2>Resumo Geral</h2>
+        <PieChart />
+      </section>
+      <section className="ultimas-compras">
+        <h2>Últimas Compras</h2>
+      </section>
+    </ModulePage>
+  );
 };
 
 export default Dashboard;
